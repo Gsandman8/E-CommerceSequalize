f394234f92bd08297bc3d209b6679b772394fcd2
Gsandman8 <gage.e.sandman@gmail.com>
2023-12-30 14:52:19 -0800

    Linked sequelize connection to server.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,26 +3,45 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  const categoriesData = await Category.findAll({
+    include: [{model: Product}],
+  });
+  res.json(categoriesData).status(200);
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  const categoryData = await Category.findByPk(req.params.id, {
+    include: [{model: Product}],
+  });
+  res.json(categoryData).status(200);
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  const categoryData = await Category.create(req.body);
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  const categoryData = await Category.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  const categoryData = await Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  });
 });
 
 module.exports = router;
